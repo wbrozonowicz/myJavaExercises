328c17dda59b8537bdca54050b6fcc9601b7b88a
wbrozonowicz <44707884+wbrozonowicz@users.noreply.github.com>
2018-12-14 21:14:09 +0100

    Add files via upload

---
+++ Ex_00.java
@@ -0,0 +1,9 @@
+package firstSemester;
+
+public class Ex_00 {
+    public static void main(String[] args) {
+        System.out.println("Hello world :-)");
+        System.out.println("Hello world :-)");
+        System.out.println("Hello world :-)");
+    }
+}
